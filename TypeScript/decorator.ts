65247dc4fe0ac0f0ead755a4294db71220f4d41e
S-a-i-f-A-l-i <saifali27906@gmail.com>
2023-05-29 21:31:30 +0530

    decorator

---
+++ TypeScript/decorator.ts
@@ -0,0 +1,20 @@
+function MenuItem(itemID:string){
+    return (target: Function) => {
+        target.prototype.id = itemID;
+    }
+    
+}
+
+@MenuItem("abc")
+class Pizza{
+    id:string;
+}
+
+
+// @MenuItem('xyz')
+// class Burger{
+//     id:string;
+// }
+
+// console.log(new Pizza.id());
+console.log(new Pizza());
